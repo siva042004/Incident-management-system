d548c408d0bc464d87943f7709c8733877952538
Siva Ragul <msan6848@gmail.com>
2026-05-29 20:54:50 +0530

    Add getById to incidentAPI

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -13,6 +13,7 @@ const api = axios.create({
 
 export const incidentAPI = {
   getAll: () => api.get("/incidents"),
+  getById: (id) => api.get(`/incidents/${id}`),
   create: (data) => api.post("/incidents", data),
   update: (id, data) => api.put(`/incidents/${id}`, data),
   delete: (id) => api.delete(`/incidents/${id}`),
